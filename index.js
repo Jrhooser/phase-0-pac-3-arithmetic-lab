70e1c047d337f519e95c86d382d004bf83d188f4
Jacob Hooser <jacob.hooser@yahoo.com>
2022-02-14 14:59:52 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+function add() {
+    return 1+1;
+}
+function subtract() {
+    return 1-1;
+}
+function multiply() {
+    return 1*1;
+}
+function divide() {
+    return 1/1;
+}
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n) {
+    return ++n;
+}
+function decrement(n) {
+    return --n;
+}
+function makeInt(n) {
+    n = parseInt(n, 10);
+    return n;
+}
+function preserveDecimal(n) {
+    n = parseFloat(n);
+    return n;
+}
